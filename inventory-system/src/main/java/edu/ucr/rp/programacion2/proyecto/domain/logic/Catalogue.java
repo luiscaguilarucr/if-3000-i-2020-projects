e80fea08b8d83c745a34c59aea32e1b50310d65b
Jeison Araya <jeison.arayamena@ucr.ac.cr>
2020-05-06 00:52:54 -0600

    update domain.logic.uxf

---
+++ inventory-system/src/main/java/edu/ucr/rp/programacion2/proyecto/domain/logic/Catalogue.java
@@ -0,0 +1,4 @@
+package edu.ucr.rp.programacion2.proyecto.domain.logic;
+
+public class Catalogue {
+}
---
+++ inventory-system/src/main/java/edu/ucr/rp/programacion2/proyecto/util/files/Utility.java
@@ -0,0 +1,4 @@
+package edu.ucr.rp.programacion2.proyecto.util.files;
+
+public class Utilty {
+}
