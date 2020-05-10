3a7512e49b580b0f84a3504c4a0d061e3401ce8f
Jeison Araya <jeison.arayamena@ucr.ac.cr>
2020-05-10 13:34:07 -0600

    Catalog modified

--- inventory-system/src/main/java/edu/ucr/rp/programacion2/proyecto/domain/logic/Catalog.java
+++ inventory-system/src/main/java/edu/ucr/rp/programacion2/proyecto/domain/logic/Catalog.java
@@ -1,19 +1,19 @@
 package edu.ucr.rp.programacion2.proyecto.domain.logic;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class Catalog {
 
     private String name;
-    private Item items;
-    private List<Items> properties;
+    private List<Item> items;
+    private List<Feature> properties;//TODO revisar properties.
 
     /**
      *
      * @param name name of the catalogue
      * @param properties properties that are going to define the items.
      */
-    public Catalog(String name, List<Items> items, ArrayList<Feature> properties) {//TODO valorar el uso de Item...items
+    public Catalog(String name, List<Item> items, List<Feature> properties) {//TODO valorar el uso de Item...items
         this.name = name;
         this.items = items;
         this.properties = properties;
@@ -23,7 +23,7 @@ public class Catalog {
         return name;
     }
 
-    public List<Items> getProperties() { //TODO valorar un retorno Feature[].
+    public List<Feature> getProperties() { //TODO valorar un retorno Feature[].
         return properties;
     }
 }
