651846ba81d9e7975d6a71b7b65b1f9035dacf98
Jeison Araya <jeison.arayamena@ucr.ac.cr>
2020-05-26 23:39:48 -0600

    javafx module solved

--- inventory-system/src/main/java/edu/ucr/rp/programacion2/proyecto/main/Main.java
+++ inventory-system/src/main/java/edu/ucr/rp/programacion2/proyecto/main/Main.java
@@ -3,7 +3,6 @@ package edu.ucr.rp.programacion2.proyecto.main;
 public class Main {
 
     public static void main(String...args){
-        System.out.println("Hello word");
         App app = new App();
     }
 
